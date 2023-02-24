75dda02a5e91096f180412f799691a6e7df58e23
hafatsat-anki <119545964+hafatsat-anki@users.noreply.github.com>
2023-02-24 04:04:57 +0200

    first version

---
+++ open_profile/__init__.py
@@ -0,0 +1,14 @@
+# Copyright: 2023 - roombon
+# License: AGPLv3+
+
+from aqt import mw
+from aqt.gui_hooks import *
+
+
+def showProfileManager() -> None:
+    if len(mw.pm.profiles()) > 1:
+        aqt.AnkiQt.unloadProfile(mw, mw.showProfileManager)
+
+
+main_window_did_init.append(showProfileManager)
+
